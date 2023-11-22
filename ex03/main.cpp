22003d34bec8becfada176ae057b0b4f1f7a80c7
abdelkarim <hajji.karim.solicode@gmail.com>
2023-11-22 18:21:24 +0100

    i finishe ex03 and just still the makefile

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ahajji <ahajji@student.1337.ma>            +#+  +:+       +#+        */
+/*   By: ahajji <ahajji@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 15:34:02 by ahajji            #+#    #+#             */
-/*   Updated: 2023/11/22 15:25:46 by ahajji           ###   ########.fr       */
+/*   Updated: 2023/11/22 18:20:06 by ahajji           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int main( void )
     // club.setType("some other type of club");
     // bob.attack();
 
-    Weapon club = Weapon("crude spiked club");
+    Weapon club = Weapon("sla7 nadi");
     HumanB jim("Jim");
     jim.setWeapon(&club);
     jim.attack();
