7e9069b64268bda53c2be9527b0eda95de0e3b9f
abdelkarim hajji <hajji.karim.solicode@gmail.com>
2023-11-24 17:07:06 +0100

    push this update

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,30 +6,32 @@
 /*   By: ahajji <ahajji@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 18:49:38 by ahajji            #+#    #+#             */
-/*   Updated: 2023/11/24 10:53:56 by ahajji           ###   ########.fr       */
+/*   Updated: 2023/11/24 17:06:34 by ahajji           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
 
+
 void    writeInsideFolderRplace(std::string fileName, std::string s1, std::string s2)
 {
     std::string line;
     std::string str;
-    std::ifstream Myfile(fileName);
     size_t pos;
     size_t len;
+    int i = 0;
+    
+    std::ifstream Myfile(fileName);
     if(Myfile.is_open())
     {
-        std::ofstream Myfile2("karim");
+        std::ofstream Myfile2(fileName + ".replace");
         while (getline(Myfile, line))
         {
             str += line;
             if(!Myfile.eof())
                 str += '\n';
         }
-        int i = 0;
         while (i < str.length())
         {
             pos = str.find(s1);
@@ -38,12 +40,12 @@ void    writeInsideFolderRplace(std::string fileName, std::string s1, std::strin
                 str.erase(pos, s1.length());
                 str.insert(pos, s2);
             }
-            i++;
+            i += s2.length();
         }
-            Myfile2 << str;
-        
+        Myfile2 << str;
     }
 }
+
 int main(int ac, char **av)
 {
     if (ac != 4)
