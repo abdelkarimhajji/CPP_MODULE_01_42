8f663ea93baa429ee7ab2a966acae07d079aeb09
abdelkarim hajji <hajji.karim.solicode@gmail.com>
2023-11-24 11:20:46 +0100

    i fixed problem in ex04 of new line to add eof()

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -3,18 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ahajji <ahajji@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ahajji <ahajji@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 18:49:38 by ahajji            #+#    #+#             */
-/*   Updated: 2023/11/22 19:28:15 by ahajji           ###   ########.fr       */
+/*   Updated: 2023/11/24 10:53:56 by ahajji           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <fstream>
 
 void    writeInsideFolderRplace(std::string fileName, std::string s1, std::string s2)
 {
-    
+    std::string line;
+    std::string str;
+    std::ifstream Myfile(fileName);
+    size_t pos;
+    size_t len;
+    if(Myfile.is_open())
+    {
+        std::ofstream Myfile2("karim");
+        while (getline(Myfile, line))
+        {
+            str += line;
+            if(!Myfile.eof())
+                str += '\n';
+        }
+        int i = 0;
+        while (i < str.length())
+        {
+            pos = str.find(s1);
+            if(pos <= str.length())
+            {
+                str.erase(pos, s1.length());
+                str.insert(pos, s2);
+            }
+            i++;
+        }
+            Myfile2 << str;
+        
+    }
 }
 int main(int ac, char **av)
 {
